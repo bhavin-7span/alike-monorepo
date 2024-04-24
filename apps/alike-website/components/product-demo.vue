a6d8dbfd1b81908e7267aba3bb6a4fea9c8fe1c1
tapan-7span <tapan@7span.com>
2024-04-24 16:12:42 +0530

    home page structure WIP

---
+++ apps/alike-website/components/product-demo.vue
@@ -0,0 +1,33 @@
+<template>
+  <Section name="hero">
+    <p>Product Detail:</p>
+    <pre>{{ productsData }}</pre>
+  </Section>
+</template>
+
+<script setup>
+import Section from "components/common/section.vue";
+import products from "../../../packages/graph/products.gql";
+const productsData = ref(null);
+const { data, errors } = await useAsyncQuery({
+  query: products,
+  variables: {
+    filter: { sku: { eq: "OneAbuDhabi" } },
+  },
+  context: {
+    headers: {
+      store: `abudhabi_en`,
+      "Content-Currency": "USD",
+    },
+    fetchOptions: { method: "GET" },
+  },
+});
+
+if (errors) {
+  console.error("Error fetching data:", errors);
+}
+
+if (data.value && data.value.products.items.length) {
+  productsData.value = data.value.products.items[0];
+}
+</script>
---
+++ apps/alike-website/pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="index-page">
+    <Header />
+    <HomeCover />
+    <HomeCuratedPackages />
+    <!-- <ProductDemo /> -->
+  </div>
+</template>
+<script setup>
+import Header from "components/header.vue";
+</script>
