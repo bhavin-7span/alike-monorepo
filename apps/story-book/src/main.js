b20b4a5bde1daeaa484fd84e1aeeebbd5a16ed60
nayan-7span <nayan@7span.com>
2024-05-08 18:17:17 +0530

    ready for dev server.

--- apps/story-book/src/main.js
+++ apps/story-book/src/main.js
@@ -1,10 +1,10 @@
-// import "./style.css";
-// import { createApp } from "vue";
-// import App from "./App.vue";
-// import { register } from "./global";
+import "./style.css";
+import { createApp } from "vue";
+import App from "./App.vue";
+import { register } from "./global";
 
-// const app = createApp(App);
-// app.mount("#app");
+const app = createApp(App);
+app.mount("#app");
 
-// //register global components
-// register(app);
+//register global components
+register(app);
